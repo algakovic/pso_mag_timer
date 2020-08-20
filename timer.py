b8e11024b72b65d5359788d88152ed306a25dcfd
Aleksandar Gakovic <algakovic@gmail.com>
2020-08-20 16:42:59 +0100

    create first timer

--- timer.py
+++ timer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 # A simple timer program used to feed Mags
 
-print('Hello')
+import time
+min = int(input('>>'))
+while min > 0:
+    print(f"{min} minutes left..")
+    time.sleep(60) # every minute
+    min -= 1 # remove one minute 
 
